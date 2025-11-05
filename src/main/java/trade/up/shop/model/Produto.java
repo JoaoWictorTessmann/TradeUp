b9f49fbdcfa2c82b273ac33b8bc7d7e30dc59f43
JoaoWictorTessmann <joaowictortessmann97@gmail.com>
2025-11-04 21:49:21 -0300

    feat(model): criar classes Categoria e Produto com @Entity

---
+++ src/main/java/trade/up/shop/model/Produto.java
@@ -0,0 +1,63 @@
+package trade.up.shop.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Produto {
+    
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+
+    private Long id;
+    private String nome;
+    private Double preco;
+    private String descricao;
+
+    @ManyToOne
+    private Categoria categoria;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+}
